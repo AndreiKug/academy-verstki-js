b7a1d6455b50c19d31c78614c1e40fa195159fa0
Anarki2D2 <anarki3@yandex.ru>
2019-01-19 19:49:16 +0400

    Slider Clothes

--- js/script.js
+++ js/script.js
@@ -31,6 +31,8 @@ let personSkin = document.getElementById('person-skin');
 //SLIDER
 //Написать через this
 // Для this переименовать классы на slider-man/slider-woman?
+
+//Hair
 let sliderSlides = document.getElementsByClassName('slider-slides');
 let slides = document.getElementsByClassName('hair-style'); // slides[5] - maximum
 let slideIndex = 0;
@@ -38,15 +40,89 @@ let prev = document.querySelector('.hair .prev');
 let next = document.querySelector('.hair .next');
 let personHair = document.getElementById('person-hair');
 
-//Для отображения нужного слайда по полу
+//Skin
+let mainPersonSkin = document.getElementById('person-skin');
+let personSkinUrl = getComputedStyle(mainPersonSkin).backgroundImage;
+
+//Clothes
+let slidesClothes = document.getElementsByClassName('clothes-style');
+let slideIndexClothes = 0;
+let prevClothes = document.querySelector('.clothes .prev');
+let nextClothes = document.querySelector('.clothes .next');
+let personClothes = document.getElementById('person-clothes');
+
+
+//Slider Clothes
+nextClothes.addEventListener('click', function () {
+    slideIndexClothes++;
+
+    for (let i = 0; i < slidesClothes.length ; i++) {
+        slidesClothes[i].style.display = 'none';
+    }
+
+    if (slideIndexClothes === slidesClothes.length) { //slides.length = 6 // slides[5] - maximum
+        slideIndexClothes = 0;
+    }
+
+    let clickEvent = new Event('click');
+    if (man.checked && slidesClothes[slideIndexClothes].classList.contains('clothes-woman')) {
+        nextClothes.dispatchEvent(clickEvent);
+    }
+    if (woman.checked && slidesClothes[slideIndexClothes].classList.contains('clothes-man')) {
+        nextClothes.dispatchEvent(clickEvent);
+    }
+
+    slidesClothes[slideIndexClothes].style.display = 'block';
+
+    let clothesUrl = getComputedStyle(slidesClothes[slideIndexClothes]).backgroundImage.replace("slide", "construct");
+    personClothes.style.backgroundImage = clothesUrl;
+});
+
+prevClothes.addEventListener('click', function () {
+    slideIndexClothes--;
+
+    for (let i = 0; i < slidesClothes.length ; i++) {
+        slidesClothes[i].style.display = 'none';
+    }
+
+    if (slideIndexClothes < 0) { //slides.length = 6 // slides[5] - maximum
+        slideIndexClothes = (slidesClothes -1);
+    }
+
+    let clickEvent = new Event('click');
+    if (man.checked && slidesClothes[slideIndexClothes].classList.contains('clothes-woman')) {
+        prevClothes.dispatchEvent(clickEvent);
+    }
+    if (woman.checked && slidesClothes[slideIndexClothes].classList.contains('clothes-man')) {
+        prevClothes.dispatchEvent(clickEvent);
+    }
+
+    slidesClothes[slideIndexClothes].style.display = 'block';
+
+    let clothesUrl = getComputedStyle(slidesClothes[slideIndexClothes]).backgroundImage.replace("slide", "construct");
+    personClothes.style.backgroundImage = clothesUrl;
+});
+
+
+
+//RadioButtons. Для отображения нужного слайда по полу
 man.addEventListener('click', function () {
+    //Проверка на радиобатон выполняется в слайдере. Поэтому выполняем событие клик.
     let clickEvent = new Event('click');
     next.dispatchEvent(clickEvent);
+    nextClothes.dispatchEvent(clickEvent);
+    //Замена скина персоны
+    mainPersonSkin.style.backgroundImage = personSkinUrl.replace("woman", "man");
 });
 woman.addEventListener('click', function () {
+    //Проверка на радиобатон выполняется в слайдере. Поэтому выполняем событие клик.
     let clickEvent = new Event('click');
     next.dispatchEvent(clickEvent);
+    nextClothes.dispatchEvent(clickEvent);
+    //Замена скина персоны
+    mainPersonSkin.style.backgroundImage = personSkinUrl.replace("man", "woman");
 });
+
 
 
 next.addEventListener('click', function () {
@@ -112,9 +188,5 @@ prev.addEventListener('click', function () {
 
 
 
-//Проверка
-/*if (man.checked &&  slides[i].classList.contains('hair-woman')) { //slides[slideIndex - 1].classList.contains('hair-woman'))
-    // i++ вернуться к циклу
-    plusSlides();
-}*/
+
 
